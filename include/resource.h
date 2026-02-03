464b336b9f74a40cb6a99db18096fac3c817b744
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2026-02-03 17:48:12 +0600

    Fix find/replace state storage

--- include/resource.h
+++ include/resource.h
@@ -2,19 +2,46 @@
 #define RESOURCE_H
 
 // Menu identifiers
-#define IDM_FILE_NEW           101
-#define IDM_FILE_OPEN          102
-#define IDM_FILE_SAVE          103
-#define IDM_FILE_EXIT          104
-#define IDM_EDIT_CUT           201
-#define IDM_EDIT_COPY          202
-#define IDM_EDIT_PASTE         203
-#define IDM_EDIT_SELECT        204
-#define IDM_EDIT_UNDO          205
-#define IDM_FILE_SAVEAS        206
-#define IDM_TOGGLE_ALWAYSONTOP 207
-#define IDM_LANG_ENGLISH       208
-#define IDM_LANG_BANGLA        209
+#define IDM_FILE_NEW            101
+#define IDM_FILE_OPEN           102
+#define IDM_FILE_SAVE           103
+#define IDM_FILE_EXIT           104
+#define IDM_FILE_PRINT          105
+#define IDM_FILE_EXPORT_PDF     106
+#define IDM_FILE_SAVEAS         107
+#define IDM_FILE_NEW_TAB        108
+#define IDM_FILE_CLOSE_TAB      109
+
+#define IDM_EDIT_CUT            201
+#define IDM_EDIT_COPY           202
+#define IDM_EDIT_PASTE          203
+#define IDM_EDIT_SELECT         204
+#define IDM_EDIT_UNDO           205
+#define IDM_EDIT_FIND           206
+#define IDM_EDIT_REPLACE        207
+#define IDM_EDIT_GOTO           208
+
+#define IDM_TOGGLE_ALWAYSONTOP  301
+#define IDM_VIEW_SYNTAX_TOGGLE  302
+#define IDM_VIEW_LANG_PLAIN     303
+#define IDM_VIEW_LANG_C         304
+
+#define IDM_OPTIONS_AUTOSAVE    401
+#define IDM_OPTIONS_LINE_CRLF   402
+#define IDM_OPTIONS_LINE_LF     403
+#define IDM_OPTIONS_ENC_UTF8    404
+#define IDM_OPTIONS_ENC_UTF16LE 405
+#define IDM_OPTIONS_ENC_UTF16BE 406
+#define IDM_OPTIONS_ENC_ANSI    407
+
+#define IDM_LANG_ENGLISH        501
+#define IDM_LANG_BANGLA         502
+
+#define IDM_RECENT_FILE_BASE    600
+#define IDM_RECENT_FILE_MAX     604
+
+#define IDD_GOTO_DIALOG         700
+#define IDC_GOTO_EDIT           701
 
 // Resource IDs
 #define IDR_MYMENU          301
